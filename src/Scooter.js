5f4378898af4156d4450306bc347b0781a405f70
Paul Preda <ppd1512@gmail.com>
2023-04-27 16:03:45 +0100

    wrote the scooter and user .js files

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,27 @@
 class Scooter{
-  // scooter code here
+  constructor(station) {
+    this.station = station;
+    this.user = null;
+    this.serial = 1;
+    this.charge = 100;
+    this.isBroken = false;
+  }
+
+  rent(user) {
+    if (this.charge < 20) {
+      throw new Error("Scooter needs to charge.");
+    } else if (this.isBroken) {
+      throw new Error("Scooter needs repair.");
+    } else {
+      this.user = user;
+      this.station = null;
+    }
+  }
+
+  dock(station) {
+    this.station = station;
+    this.user = null;
+  }
 }
 
 
